d4c9a75ee0f945b73b8deeaf119ff67c5f43bf6c
Mohammadreza nazari <m_nazari@arsh.co>
2022-08-12 17:58:27 +0430

    main models added

--- server/main/models.py
+++ server/main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class House(models.Model):
+    post_number = models.CharField(max_length=16, unique=True, verbose_name=_('post_number'))
+    address = models.CharField(max_length=255, null=True, verbose_name=_('address'))
+    area = models.IntegerField(null=True, verbose_name=_('area'))
+    price = models.IntegerField(null=True, verbose_name=_('price'))
+
+    class Meta:
+        verbose_name = _('house')
+        verbose_name_plural = _('houses')
+
+
+class Vehicle(models.Model):
+    manufacture_id = models.CharField(max_length=20, unique=True, verbose_name=_('manufacture_id'))
+    price = models.IntegerField(null=True, verbose_name=_('price'))
+
+    class Meta:
+        verbose_name = _('vehicle')
+        verbose_name_plural = _('vehicles')
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=255, null=True, verbose_name=_('name'))
+    phone_number = models.CharField(max_length=15, null=True, verbose_name=_('phone_number'))
+    national_id = models.CharField(max_length=20, unique=True, verbose_name=_('national_id'))
+
+    class Meta:
+        verbose_name = _('person')
+        verbose_name_plural = _('persons')
+    
