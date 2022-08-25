4749427287fd2e1e08b0496c8d65abdbd8726121
Mohammadreza nazari <m_nazari@arsh.co>
2022-08-25 13:45:29 +0430

    admin classes added

--- server/main/admin.py
+++ server/main/admin.py
@@ -2,7 +2,61 @@ from django.contrib import admin
 
 from .models import House, Person, Vehicle, Relation
 
-admin.site.register(House)
-admin.site.register(Person)
-admin.site.register(Vehicle)
-admin.site.register(Relation)
+
+class HouseAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'post_number',
+        'price',
+    )
+    list_filter = (
+        'post_number',
+        'price',
+    )
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'phone_number',
+        'national_id',
+    )
+    list_filter = (
+        'name',
+        'phone_number',
+        'national_id',
+    )
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'manufacture_id',
+        'price',
+    )
+    list_filter = (
+        'manufacture_id',
+        'price',
+    )
+
+
+class RelationAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_node_type',
+        'first_node_id',
+        'second_node_type',
+        'second_node_id',
+    )
+    list_filter = (
+        'first_node_type',
+        'first_node_id',
+        'second_node_type',
+        'second_node_id',
+    )
+
+
+admin.site.register(House, HouseAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
+admin.site.register(Relation, RelationAdmin)
