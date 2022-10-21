c98be7bdd7961931aceddda4c78e7a12c9e5b65d
محمد حسین حیدرزاده <79692434+heidarzade-mh@users.noreply.github.com>
2022-10-21 21:00:52 +0330

    fix html bug

--- front-end/src/app/view/explorer-view/explorer-view.module.ts
+++ front-end/src/app/view/explorer-view/explorer-view.module.ts
@@ -1,23 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {ExplorerViewComponent} from './explorer-view.component';
 import {DatasetApiService} from '../../api/dataset-api.service';
 import {NzCardModule} from 'ng-zorro-antd/card';
 import {NzDividerModule} from 'ng-zorro-antd/divider';
 import {NzTableModule} from 'ng-zorro-antd/table';
 import {HttpClientModule} from '@angular/common/http';
-
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+/** config angular i18n **/
+import {registerLocaleData} from '@angular/common';
+import en from '@angular/common/locales/en';
+registerLocaleData(en);
+/** config ng-zorro-antd i18n **/
+import {NZ_I18N, en_US} from 'ng-zorro-antd/i18n';
 
 @NgModule({
-  declarations: [ExplorerViewComponent],
-  imports: [
-    CommonModule,
-    NzCardModule,
-    NzTableModule,
-    NzDividerModule,
-    HttpClientModule,
-  ],
-  providers: [DatasetApiService]
+    declarations: [ExplorerViewComponent],
+    imports: [CommonModule, NzCardModule, NzTableModule, NzDividerModule, HttpClientModule, BrowserAnimationsModule],
+    providers: [DatasetApiService, {provide: NZ_I18N, useValue: en_US}],
 })
-export class ExplorerViewModule { }
+export class ExplorerViewModule {}
