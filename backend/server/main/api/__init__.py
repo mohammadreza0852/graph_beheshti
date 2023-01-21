b188352bce347a257424f076e5e6b4835cb4eb14
Mohammadreza nazari <m_nazari@arsh.co>
2023-01-21 21:49:38 +0330

    node recommandation based on bfs

--- backend/server/main/api/__init__.py
+++ backend/server/main/api/__init__.py
@@ -1,4 +1,4 @@
-from .graph import GraphViewSet, NodeView, ExpandView
+from .graph import GraphViewSet, NodeView, ExpandView, RecommandNodeView
 from .base import DatasetView, PropertyView
 from .media import NodeImageView
 from .filters import CustomFilterView, NodeCustomFilterView
